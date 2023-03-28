0fbc1f99b888e500096b28ad3457f44296db8526
kiokroel <125490038+kiokroel@users.noreply.github.com>
2023-03-28 18:14:33 +0600

    Add files via upload

---
+++ olympiad_tasks/Company_XXX.py
@@ -0,0 +1,36 @@
+f = open('D:/Company_XXX.txt')
+
+persons = []  # список сотрудников
+
+# формирование списка
+string = f.readline().split('\n')[0]
+while string != 'END':
+    persons.append(string)
+    string = f.readline().split('\n')[0]
+
+# начальник
+chief = f.readline().split('\n')[0]
+
+# номер + имя начальника
+for i in range(len(persons)):
+    if chief in persons[i] and len(persons[i]) > len(chief):
+        chief = persons[i]
+        break
+
+
+subordinates = [chief]
+for sub in subordinates:
+    for i in range(len(persons)//2):
+        if persons[i * 2] in sub or sub in persons[i * 2]:
+            subordinates.append(persons[i * 2 + 1])
+            if len(sub) < len(persons[i * 2]):
+                subordinates[subordinates.index(sub)] = persons[i * 2]
+subordinates.pop(0)
+subordinates.sort()
+
+
+for sub in subordinates:
+    if len(sub) == 4:
+        print(sub + ' Unknown Name')
+    else:
+        print(sub)
