5d2c0cdc87e0d078b785a91138c982c592aa192c
kiokroel <125490038+kiokroel@users.noreply.github.com>
2023-04-27 15:32:03 +0600

    Update List.cs

--- algorithmization/Lab_3-4_Сollections/List.cs
+++ algorithmization/Lab_3-4_Сollections/List.cs
@@ -12,11 +12,11 @@ void print(string name, List<string> list)
 List<string> list = new List<string>();
 while (true)
 {
-    Console.WriteLine("Ìåíþ \n\n1.Äîáàâèòü ýëåìåíò â êîëëåêöèþ \n2.Äîáàâèòü â ñïèñîê ýëåìåíòû èç êîëëåêöèè c \n3.Àëãîðèòì äâîè÷íîãî ïîèñêà \n4.Èíäåêñ ïåðâîãî âõîæäåíèÿ îáúåêòà \n5.Èçìåíåíèå ïîðÿäêà ýëåìåíòîâ íà îáðàòíûé \n6.Ñîðòèðîâêà \n7.Î÷èùåíèå êîëëåêöèè \n8.Êîëè÷åñòâî îáúåêòîâ, õðàíÿùèõñÿ â êîëëåêöèè \n9.Óäàëèòü ÷àñòü êîëëåêöèè \n10.Âûõîä");
+    Console.WriteLine("Меню \n\n1.Добавить элемент в коллекцию \n2.Добавить в список элементы из коллекции c \n3.Алгоритм двоичного поиска \n4.Индекс первого вхождения объекта \n5.Изменение порядка элементов на обратный \n6.Сортировка \n7.Очищение коллекции \n8.Количество объектов, хранящихся в коллекции \n9.Удалить часть коллекции \n10.Выход");
     string key = Console.ReadLine();
     if (key == "1")
     {
-        Console.WriteLine("Ââåäèòå ýëåìåíò");
+        Console.WriteLine("Введите элемент");
         list.Add(Console.ReadLine());
         print("list", list);
     }
@@ -24,11 +24,11 @@ while (true)
     {
         List<string> c = new List<string>();
         string key_2;
-        Console.WriteLine("Ââåäèòå ýëåìåíòû êîëëåêöèè c");
+        Console.WriteLine("Введите элементы коллекции c");
         while (true)
         {
             c.Add(Console.ReadLine());
-            Console.WriteLine("1.Ïðîäîëæèòü \n2.Âûõîä");
+            Console.WriteLine("1.Продолжить \n2.Выход");
             key_2 = Console.ReadLine();
             if (key_2 == "1")
             {
@@ -40,7 +40,7 @@ while (true)
             }
             else
             {
-                Console.WriteLine("Íåèçâåñòíàÿ êîìàíäà");
+                Console.WriteLine("Неизвестная команда");
                 break;
             }
             Console.Clear();
@@ -52,8 +52,8 @@ while (true)
     }
     else if (key == "3")
     {
-        Console.WriteLine("Óêàæèòå ýëåìåíò");
-        Console.WriteLine("Èíäåêñ ýëåìåíòà: " + list.BinarySearch(Console.ReadLine()));
+        Console.WriteLine("Укажите элемент");
+        Console.WriteLine("Индекс элемента: " + list.BinarySearch(Console.ReadLine()));
     }
     else if (key == "4")
     {
@@ -80,7 +80,7 @@ while (true)
     }
     else if (key == "9")
     {
-        Console.WriteLine("Ââåäèòå íà÷àëüíûé èíäåêñ è êîëè÷åñòâî ýëåìåíòîâ êîòîðûå âû õîòèòå óäàëèòü");
+        Console.WriteLine("Введите начальный индекс и количество элементов которые вы хотите удалить");
         list.RemoveRange(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
         print("list", list);
     }
@@ -90,9 +90,9 @@ while (true)
     }
     else
     {
-        Console.WriteLine("Íåèçâåñòíàÿ êîìàíäà");
+        Console.WriteLine("Неизвестная команда");
     }
-    Console.WriteLine("Íàæìèòå ëþáóþ êëàâèøó");
+    Console.WriteLine("Нажмите любую клавишу");
     Console.ReadKey();
     Console.Clear();
 }
