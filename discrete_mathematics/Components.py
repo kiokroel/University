b152e3e14aee77d1ec9a827777f0e4b5acea6f16
kiokroel <125490038+kiokroel@users.noreply.github.com>
2023-06-04 18:06:13 +0600

    Update Components.py

--- discrete_mathematics/Components.py
+++ discrete_mathematics/Components.py
@@ -5,24 +5,25 @@ matrix = [[1, 0, 1, 0, 0, 0],
           [0, 1, 0, 0, 1, 0],
           [0, 0, 1, 1, 0, 1]]
 
-nodes = '123456'
+nodes = list()
+for i in range(len(matrix)):
+    nodes.append(i)
 
 components = list()
 
 while nodes:
-    node = nodes[0]
-    currents_comp = str(node)
-    nodes = nodes.replace(node, '')
-    node = int(node) - 1
+    node = nodes.pop(0)
+    currents_comp = list()
+    currents_comp.append(node + 1)
     for i in range(len(matrix)):
         if i != node and matrix[node][i] == 1:
-            currents_comp += str(i + 1)
-            nodes = nodes.replace(str(i + 1), '')
+            currents_comp.append(i + 1)
+            nodes.remove(i)
     for nod in currents_comp:
         for i in range(len(matrix)):
-            if matrix[int(nod)-1][i] == 1:
-                currents_comp += str(i + 1)
-                nodes = nodes.replace(str(i + 1), '')
+            if matrix[nod-1][i] == 1 and (i + 1 not in currents_comp):
+                currents_comp.append(i + 1)
+                nodes.remove(i)
     components.append(set(currents_comp))
 
 print(components)
